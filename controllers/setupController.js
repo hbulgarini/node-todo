796e05ac5ffb01c0a195686c07d608bcf1e4996a
root <root@ip-172-31-40-219.us-west-2.compute.internal>
2017-02-07 21:40:12 +0000

    first commit

---
+++ controllers/setupController.js
@@ -0,0 +1,31 @@
+var Todos= require('../models/todoModel');                                                                                                                                                              
+                                                                                                                                                                                                        
+module.exports = function(app){                                                                                                                                                                         
+                                                                                                                                                                                                        
+  app.get('/api/setupTodos',function(req,res) {                                                                                                                                                          
+                                                                                                                                                                                                        
+    var starterTodos = [                                                                                                                                                                                  
+      {                                                                                                                                                                                                 
+        username:'test',                                                                                                                                                                                
+        todo:'buy milk',                                                                                                                                                                                
+        isDone:false,                                                                                                                                                                                   
+        hasAttachment:false                                                                                                                                                                             
+      },                                                                                                                                                                                                
+      {                                                                                                                                                                                                 
+        username:'test',                                                                                                                                                                                
+        todo:'Feed dog',                                                                                                                                                                                
+        isDone:false,                                                                                                                                                                                   
+        hasAttachment:false                                                                                                                                                                             
+      },                                                                                                                                                                                                
+      {                                                                                                                                                                                                 
+        username:'test',                                                                                                                                                                                
+        todo:'Study node',                                                                                                                                                                              
+        isDone:false,                                                                                                                                                                                   
+        hasAttachment:false                                                                                                                                                                             
+      }                                                                                                                                                                                                 
+    ];                                                                                                                                                                                                                                                                                                                                                                                                      
+    Todos.create(starterTodos,function(err,results){                                                                                                                                                      
+    res.send(results);                                                                                                                                                                                  
+    }); 
+  })
+}
